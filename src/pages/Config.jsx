c406432b4301f4dbe6451320154d9e8e953f7929
cris-dangithub <cristiandaniel8080@gmail.com>
2022-12-27 15:47:33 -0500

    darkmode

--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -6,16 +6,20 @@ import { setPokePerPageGlobal } from '../store/slices/pokePerPage.slice'
 import './styles/Config.css'
 
 const Config = () => {
+  const rootReact = document.querySelector('#root')
   const { darkmode, pokePerPage } = useSelector(state => state)
   const dispatch = useDispatch()
   const handleClickDarkMode = () => {
     darkmode ? dispatch(darkMode()) : dispatch(lightMode())
+    rootReact.classList.toggle('u-dark-mode')
   }
 
   const availableOptions = [4, 8, 12, 16, 20]
   const handlePokePerPages = (option) => {
     dispatch(setPokePerPageGlobal(option))
+    
   }
+
 
   return (
     <article className='c-config'>
@@ -23,7 +27,7 @@ const Config = () => {
         <h1 className='config-card__title'>Configure options</h1>
         <ul className='config-card__list'>
           <li className='config-card__item'>
-            <span className='config-card__title-option'>Darkmode (developing)</span>
+            <span className='config-card__title-option'>Darkmode</span>
             <div className='config-card__switch' onClick={handleClickDarkMode}>
               <div className={`config-card__switch-btn ${darkmode ? 'config-card__switch-btn--active' : ''}`}></div>
             </div>
